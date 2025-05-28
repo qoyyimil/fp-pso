69f246b9be01a9528e5b7a2b55faade1dcf4a4c3
qoyyimil <qoyyimillj@gmail.com>
2025-05-28 19:58:20 +0700

    fix: Remove redundant extensionsToTreatAsEsm from Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,7 @@
-// jest.config.js
-/** @type {import('jest').Config} */
+@type {import('jest').Config} */
 const config = {
-  // Memberi tahu Jest untuk memperlakukan file .js sebagai ES Modules
-  // Ini memberitahu Jest untuk tidak mencoba mengubah import/export statement
-  // tapi untuk menganggapnya sebagai modul ES native.
-  transform: {}, // Menonaktifkan transformasi default untuk ES Modules
-
-  // Ini penting jika Anda menggunakan sintaks import/export di file JS biasa (.js)
-  // dan tidak menggunakan ".mjs"
-  extensionsToTreatAsEsm: ['.js'],
-
-  // Pastikan Jest tahu untuk menguji file .js, .mjs, dll.
+  transform: {},
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'json', 'node'],
-
-  // Lingkungan pengujian yang mensimulasikan browser DOM
   testEnvironment: 'jsdom',
 };
 
